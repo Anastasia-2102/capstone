8d07ce26456c63c47561bce5e39ca9df27112c08
Anastasia <ketchersid18@mail.ru>
2026-09-11 11:24:11 -0500

    the first input changes the request

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let searchBox = document.getElementById("search-box");
+
+async function loadParks() {
+  let response = await fetch("https://student-data-api.anastasiaketchersid-757.workers.dev/api/v1/datasets/us-national-parks/records?search=" + searchBox.value);
+  console.log("Status: " + response.status);
+
+  let data = await response.json();
+  let parks = data.records;
+  console.log("Records: " + parks.length);
+}
+
 let goButton = document.getElementById("go-button");
 
 goButton.addEventListener("click", function () {
-  document.getElementById("results-list").textContent = searchBox.value;
+  loadParks();
 });
